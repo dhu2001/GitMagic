5b133df5ed10d01e2c8dcd5617f01f603f470ba7
Darian Hu <dhkzy@umsystem.edu>
2021-08-30 17:04:55 -0500

    added branch log

---
+++ sentences/dhkzy.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+
+// Function to include & executeA
+int dariansSentence1()
+{
+  printf("There once was a pirate named Monkey D. Luffy\n");
+  return 1;
+}
+
+int dariansSentence_FINAL()
+{
+  printf("To be continued.... \n");
+  return 1;
+}
